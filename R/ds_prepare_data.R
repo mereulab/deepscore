eee6e494fa015d80b5b4c28a29a3827f2ce84260
JessicaKLL <129850754+JessicaKLL@users.noreply.github.com>
2023-09-14 12:06:26 +0200

    Update ds_prepare_data.R

--- R/ds_prepare_data.R
+++ R/ds_prepare_data.R
@@ -50,12 +50,12 @@ ds_prepare_data<-function(ref.data,ref.assay,query.data,query.assay,markers,mark
   query_gg<-rownames(query.data)
   sel_gg<-intersect(query_gg,sel_gg)
 
-  ref.data <- GetAssayData(ref.data, slot = "data", assay = ref.assay)
   ref.data <- ref.data[sel_gg,]
+  ref.data <- GetAssayData(ref.data, slot = "data", assay = ref.assay)
   ref.data<-as.matrix(ref.data)
 
-  query.data <- GetAssayData(query.data, slot = "data", assay = query.assay)
   query.data<-query.data[sel_gg,]
+  query.data <- GetAssayData(query.data, slot = "data", assay = query.assay)
   query.data<-as.matrix(query.data)
 
   out<-list(ref.data=ref.data,query.data=query.data,ref.cluster=ref.cluster,query.cluster=query.cluster,markers=sel_gg)
