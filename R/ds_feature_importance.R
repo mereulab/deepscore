2db2bb45aec7dd5dc513a9027718b76f2fc093bc
JessicaKLL <129850754+JessicaKLL@users.noreply.github.com>
2023-10-02 17:26:58 +0200

    Add files via upload

---
+++ R/ds_feature_importance.R
@@ -0,0 +1,58 @@
+#' This function calculates the feature importance of the deep learning model
+#'
+#' This function generates a dataframe with the feature importance score of the input features calculated
+#' based on the "Permutation Feature Importance" approach.
+#'
+#' @param X The data with the feature information
+#' @param Y The data with the output information
+#' @param model The deep learning model
+#' @param metric The evaluation metric used to calculate the feature importance, "loss" or "accuracy".
+#' @param features The manually selected features whose importance will be calculated. If not provided,
+#' the function will use all the features. You can control the number of used features by using the parameter
+#' `max_n`.
+#' @param  max_n Number of features selected from X if `features` not provided.
+#'
+#' @import keras
+#' @import tensorflow
+#'
+#' @return A dataframe with the importance score of each feature
+#'
+#' @export
+#' @examples
+#' ds_feature_importance(X=x_test,Y=y_test,metric="accuracy",max_n=500)
+#'
+
+ds_feature_importance<-function(X,Y,model,metric,features,max_n=1000){
+  test_metrics<- model %>% evaluate(x=X,y=Y)
+  importance<-c()
+  if(missing(features)){
+    if(missing(max_n)){
+      features<-colnames(X)
+    }
+    else{
+    features<-colnames(X)[1:max_n]
+    }
+  }
+  if(metric=="loss"){
+    baseline<-test_metrics[1]
+    num_permutations<-length(features)
+    for (i in 1:num_permutations) {
+      xi = rep(0,nrow(X))
+      Xi = cbind(xi, X[,-which(colnames(X) == features[i])])
+      permuted_metrics <- model %>% evaluate(x = Xi, y = Y,verbose = 0)
+      importance <- c(importance,(((permuted_metrics[1]-baseline)/baseline))*100)
+    }
+  }
+  if(metric=="accuracy"){
+    baseline<-test_metrics[2]
+    num_permutations<-length(features)
+    for (i in 1:num_permutations) {
+      xi = rep(0,nrow(X))
+      Xi = cbind(xi, X[,-which(colnames(X) == features[i])])
+      permuted_metrics <- mod1 %>% evaluate(x = Xi, y = Y,verbose = 0)
+      importance <- c(importance,(((permuted_metrics[2]-baseline)/baseline))*100)
+    }
+  }
+  PFI_final = data.frame("Feature"=features,"Score" = importance)
+  return(PFI_final)
+}
---
+++ R/ds_plot_importance.R
@@ -0,0 +1,40 @@
+#' This function plots the importance of the features
+#'
+#' This function plots the n top importance features decreasing or increasing.
+#'
+#' @param data The output of `ds_feature_importance`.
+#' @param n The number of features to be plotted (Default=30)
+#' @param Decrease If the importance are plotted decreasing or increasing.
+#' @param general_alteration If plot the absolut scores, if `TRUE`, the plot would not take into account if the
+#' alteration means an increase or decrease. (Default=FALSE)
+#' @param main The title of the plot
+#'
+#' @import ggplot2
+#'
+#' @return A feature importance plot
+#'
+#' @export
+#'
+#' @examples
+#' ds_plot_importance(data,n=50,Decrease=TRUE,general_alteration=FALSE,main="Feature Importance Plot")
+#'
+
+ds_plot_importance<-function(data,n=30,Decrease=TRUE,general_alteration=FALSE,main=""){
+  rownames(data)<-NULL
+  data<-data[order(data$Score, decreasing=Decrease),]
+  if(general_alteration){
+    data$Score<-abs(data$Score)
+  }
+  data<-data[1:n,]
+  Selected_features<-data$Feature
+  plot<-ggplot(data, aes(x=reorder(Feature,Score), y=Score, fill=Score))+
+    geom_bar(stat = "identity",position="dodge")+coord_flip()+
+    ylab("Variable Importance Score") + xlab("") +
+    ggtitle(main)+
+    scale_fill_gradientn(colors = c("yellow","orange","purple"))+theme_minimal()
+  output_list<-list()
+  output_list[[1]]<-Selected_features
+  output_list[[2]]<-plot
+  names(output_list)<-c("Selected","Feature_plot")
+  return(output_list)
+}
